77b0889cc4431adb50a6595e4b5a20e0dbae9cc0
Manuela Caro Villada <104787569+ManuCarov@users.noreply.github.com>
2023-09-27 02:41:51 -0500

    pagina login

--- HUG/settings.py
+++ HUG/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
     'page',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "login/templates/static"), 
+]
+
+TAILWIND_CSS_ROOT = os.path.join(BASE_DIR, 'login/templates/static')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- HUG/urls.py
+++ HUG/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from page import views as pageViews
+from login import views as loginViews
+from django.views.static import serve
+from django.conf import settings
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', pageViews.home),
+    path('login/', loginViews.login),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.TAILWIND_CSS_ROOT})
 ]

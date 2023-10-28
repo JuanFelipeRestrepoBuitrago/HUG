fde138fe8a8fb6e1379d6cd9f4c71feacdeeb02e
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-10-28 15:58:42 -0500

    New App for Egresados

--- HUG/settings.py
+++ HUG/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuarios',
     'main',
+    'crudEgresados',
 ]
 
 AUTH_USER_MODEL = 'usuarios.CustomUser'
---
+++ crudEgresados/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrudegresadosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'crudEgresados'

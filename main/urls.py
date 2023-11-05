fc4dfd8d7e1d9bb2933eecee07f3182109a9b3e6
julianvb03 <valenciajuliann@hotmail.com>
2023-11-05 11:47:46 -0500

    Cambios

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, manual, dashboard, grafica_dinamica1, grafica_dinamica2
+from .views import home, manual, dashboard, grafica_dinamica1, grafica_dinamica2, grafica_dinamica3, grafica_dinamica4, grafica_dinamica5
 
 urlpatterns = [
     path('', home, name='inicio'),
@@ -8,5 +8,8 @@ urlpatterns = [
 
 
     path('grafica_dinamica1/', grafica_dinamica1, name='grafica_dinamica1'),
-    path('grafica_dinamica2/', grafica_dinamica2, name='grafica_dinamica2')
+    path('grafica_dinamica2/', grafica_dinamica2, name='grafica_dinamica2'),
+    path('grafica_dinamica3/', grafica_dinamica3, name='grafica_dinamica3'),
+    path('grafica_dinamica4/', grafica_dinamica4, name='grafica_dinamica4'),
+    path('grafica_dinamica5/', grafica_dinamica5, name='grafica_dinamica5')
 ]

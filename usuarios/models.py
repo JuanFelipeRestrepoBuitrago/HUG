81f70876ad9a7acc4a48770e995fb62e0492384f
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-11-05 22:59:41 -0500

    Functions Refactored
    Functions were put in their respective classes

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,13 +1,75 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Q
+from django.db import IntegrityError
 
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, max_length=254)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
-    institution = models.CharField(max_length=500)
-    user_type = models.CharField(max_length=5, default='user', choices=(('user', 'user'), ('admin', 'admin')))
+    institution = models.CharField(max_length=500, null=True, blank=True)
+    user_type = models.CharField(max_length=5, default='admin', choices=(('user', 'user'), ('admin', 'admin')))
 
     def __str__(self):
+        """
+        Esta funcion permite obtener el nombre de usuario cuando se imprime un objeto de la clase CustomUser
+        @return: retorna el nombre de usuario
+        """
         return self.username
+
+    @staticmethod
+    def iniciar_sesion(request, username, password):
+        """
+        Esta funcion permite iniciar sesion en el sistema
+
+        @param request: HttpRequest, objeto que contiene la informacion sobre la solicitud web actual
+        @param username: nombre de usuario o correo electronico
+        @param password: contraseña
+        @return:
+        """
+        # Recupera el nombre de usuario si se ingreso el correo electronico
+        username = CustomUser.objects.get(Q(username=username) | Q(email=username)).username
+        # Se autentica el usuario
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Se inicia sesion
+            login(request, user)
+            return user
+        else:
+            raise IntegrityError('Usuario o contraseña incorrectos')
+
+    @staticmethod
+    def cerrar_sesion(request):
+        """
+        Esta funcion permite cerrar sesion en el sistema
+
+        @param request: HttpRequest, objeto que contiene la informacion sobre la solicitud web actual
+        """
+
+        # Se cierra sesion
+        logout(request)
+
+    @staticmethod
+    def registrarse(request, username, email, first_name, last_name, institution, password, confirm_password):
+        """
+        Esta funcion permite registrar un usuario en el sistema
+
+        @param request: HttpRequest, objeto que contiene la información sobre la solicitud web actual
+        @param username: nombre de usuario
+        @param email: correo electrónico
+        @param first_name: Nombre del usuario
+        @param last_name: Apellido del usuario
+        @param institution: Institución a la que pertenece el usuario
+        @param password: Contraseña
+        @param confirm_password: Confirmación de la contraseña
+        """
+        if password != confirm_password:
+            raise IntegrityError('Las contraseñas no coinciden')
+        user = CustomUser(username=username, email=email, first_name=first_name, last_name=last_name,
+                          institution=institution, user_type='user', password=password)
+
+        user.save()
+        login(request, user)
+        return user
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,38 +1,88 @@
+from django.db import IntegrityError
 from django.shortcuts import render, redirect
 from .models import CustomUser
-from django.db.models import Q
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def iniciar_sesion(request):
     """
-    Esta función permite iniciar sesión en el sistema. Si el usuario ya está autenticado, se redirige a la página de inicio.
-    Si el usuario no está autenticado, se muestra el formulario de inicio de sesión. Si el usuario no existe, se muestra un
-    mensaje de error. Si el usuario existe, pero la contraseña es incorrecta, se muestra un mensaje de error.
+    Esta función permite iniciar sesión en el sistema.
 
     @param request: HttpRequest, objeto que contiene la información sobre la solicitud web actual.
-    @return: HttpResponse, objeto que contiene la respuesta HTTP que se enviará al navegador web que realizó la solicitud.
+    @return: HttpResponse, objeto que contiene la respuesta HTTP que se
+    enviará al navegador web que realizó la solicitud.
     """
+
+    # Si el usuario ya está autenticado, se redirige a la página de inicio.
     if request.user.is_authenticated:
         return redirect('inicio')
+    # Si el método de la petición es GET, se muestra el formulario de inicio de sesión.
     elif request.method == 'GET':
         return render(request, 'signin.html')
+    # Si el método de la petición es POST, se intenta iniciar sesión.
     elif request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         try:
-            username = CustomUser.objects.get(Q(username=username) | Q(email=username)).username
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                if user.user_type == 'admin':
-                    return redirect('inicio') # TODO: Cambiar a la página de administrador
-                else:
-                    return redirect('inicio')
+            usuario = CustomUser.iniciar_sesion(request, username, password)
+            # Si el usuario es un administrador, se redirige a la página de administración, de lo contrario,
+            # se redirige a la página de inicio.
+            if usuario.user_type == 'admin':
+                return redirect('inicio')  # TODO: Cambiar a admin
             else:
-                messages.error(request, 'Usuario o contraseña incorrectos')
-                return redirect('login')
+                return redirect('inicio')
         except CustomUser.DoesNotExist:
             messages.error(request, 'El usuario no existe')
             return redirect('login')
+        except IntegrityError as error:
+            messages.error(request, error)
+            return redirect('login')
+
+
+@login_required
+def cerrar_sesion(request):
+    """
+    Esta función permite cerrar sesión en el sistema.
+
+    @param request: HttpRequest, objeto que contiene la información sobre la solicitud web actual.
+    @return: HttpResponse, objeto que contiene la respuesta HTTP que se
+    enviará al navegador web que realizó la solicitud.
+    """
+
+    CustomUser.cerrar_sesion(request)
+    return redirect('inicio')
+
+
+def registrarse(request):
+    """
+    Esta función permite registrar un usuario en el sistema.
+
+    @param request: HttpRequest, objeto que contiene la información sobre la solicitud web actual.
+    @return: HttpResponse, objeto que contiene la respuesta HTTP que se
+    enviará al navegador web que realizó la solicitud.
+    """
+
+    # Si el usuario ya está autenticado, se redirige a la página de inicio.
+    if request.user.is_authenticated:
+        return redirect('inicio')
+    # Si el método de la petición es GET, se muestra el formulario de registro.
+    elif request.method == 'GET':
+        return render(request, 'signup.html')
+    # Si el método de la petición es POST, se intenta registrar el usuario.
+    elif request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        institution = request.POST['institution']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        try:
+            CustomUser.registrarse(request, username, email, first_name, last_name, institution, password,
+                                   confirm_password)
+            return redirect('inicio')
+        except IntegrityError as error:
+            messages.error(request, error)
+            return redirect('signup')

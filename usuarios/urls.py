70a1f3345075d2fcd6b60f9f51460dee3e5d2ced
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-11-05 23:01:38 -0500

    URLs for Sign Out and Sign Up

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import iniciar_sesion
+from .views import iniciar_sesion, cerrar_sesion, registrarse
 
 urlpatterns = [
     path('iniciar_sesion/', iniciar_sesion, name='login'),
+    path('cerrar_sesion/', cerrar_sesion, name='logout'),
+    path('registrarse/', registrarse, name='signup'),
 ]

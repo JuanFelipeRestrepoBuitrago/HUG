bc4f85c59302d5494c51e528cfb9be7cd1c281ec
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-11-10 21:14:47 -0500

    Deleting Unnecessary Crud JS

--- static/js/crud/egresados.js
+++ static/js/crud/egresados.js
@@ -1,3 +1,25 @@
+// Obtener la flecha de la lista desplegable
+const caret = document.querySelector('.caret');
+// Obtener el select
+const select = document.querySelector('#nivel_educativo');
+
+// Rotar la flecha de la lista desplegable al hacer clic en ella
+select.addEventListener('mousedown', () => {
+    caret.classList.toggle('caret-rotate');
+});
+
+// Esconder la flecha de la lista desplegable al hacer clic fuera de ella
+document.addEventListener("click", function (event) {
+    if (!select.contains(event.target) || event.target === caret) {
+        caret.classList.remove('caret-rotate');
+    }
+});
+
+// Esconder la flecha de la lista desplegable al seleccionar una opción
+select.addEventListener('change', () => {
+    caret.classList.remove('caret-rotate');
+});
+
 // Agregar valores al formulario de edición
 function addValues(modal, input, fechaNacimiento, nivelEducativo, salario, experienciaMeses, ciudad){
     modal.querySelector('#fecha_nacimiento').value = fechaNacimiento;
